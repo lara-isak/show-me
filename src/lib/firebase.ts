84aa0f3ee8bd7bd4e787dd419f10f9e3b5cadbf4
Lara Isak <luckasti.larry@gmail.com>
2025-04-20 09:05:41 +0200

    Store sensitive data in env vars

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCE3gdv5m4NLabSG8dVpkUrhk3GD3fIh3Y",
-  authDomain: "show-me-db80f.firebaseapp.com",
-  projectId: "show-me-db80f",
-  storageBucket: "show-me-db80f.firebasestorage.app",
-  messagingSenderId: "895588752232",
-  appId: "1:895588752232:web:14d139f2b89799d4e054dd",
-  measurementId: "G-SB0X35CZC1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
